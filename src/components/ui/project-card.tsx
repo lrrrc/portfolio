e39a7c7133d1cd6f50300fbf8debc83cf8b65238
Leonardo <leonardo.cardoso@fiap.com.br>
2023-11-07 19:34:49 -0300

    :technologist: Card changed.

--- src/components/ui/project-card.tsx
+++ src/components/ui/project-card.tsx
@@ -3,14 +3,15 @@ import { Eye } from "lucide-react";
 import { Button } from "./button";
 import { Card, CardHeader, CardTitle, CardContent, CardFooter } from "./card";
 import { Separator } from "./separator";
+import Link from "next/link";
 
 interface IRequest {
     title: string,
-    url: string,
-    content: string
+    content: string,
+    url: string
 }
 
-export default function ProjectCard({ title , url, content } : IRequest) {
+export default function ProjectCard({ title , content, url } : IRequest) {
 
     return (
         <Card className="grid grid-rows-[min-content_1fr_min-content] w-[20vw] min-h-[32vh] gap-2 flex-col p-1 mb-5  bg-neutral-700 border-none max-md:w-[72vw]">
@@ -24,9 +25,9 @@ export default function ProjectCard({ title , url, content } : IRequest) {
                 {content}
             </CardContent>
             <CardFooter className="flex justify-center">
-                <Button onClick={() => window.open(url, '_blank')} className="w-[100px] bg-metaColor-100 hover:bg-metaColor-200 text-zinc-50 gap-2">
+                <Link href={url} className="w-[100px] bg-metaColor-100 hover:bg-metaColor-200 text-zinc-50 gap-2 rounded-md flex p-2 justify-center items-center text-sm font-medium">
                     View <Eye />
-                </Button>
+                </Link>
             </CardFooter>
         </Card>
     )
