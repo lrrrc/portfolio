2131bb4225aa6412abc4fdbe20dcef107674f5fe
Leonardo <leonardo.cardoso@fiap.com.br>
2023-11-09 00:20:01 -0300

    :white_check_mark: New projects

--- src/app/projects/page.tsx
+++ src/app/projects/page.tsx
@@ -12,6 +12,16 @@ export default function Projects() {
                     content="This software is a URL shortener that facilitates the LGPD system for some documentation that I need to use on a daily basis. In this project, only JavaScript is used."
                     url="https://github.com/leozvx/encurtador-tinyurl"
                 />
+                 <ProjectCard
+                    title="Studying Guide"
+                    content="A straightforward guide to enhance your coding skills. This guide covers various programming fundamentals with a particular focus on algorithms and Front-End tips."
+                    url="https://leozvx.github.io/study-pages"
+                />
+                  <ProjectCard
+                    title="scratchend"
+                    content="A new CLI tool to save time in your back-end setup. The function of the CLI is to create all your dependencies and the basic architecture, so you can start your project with all the best practices."
+                    url="https://github.com/scratchend/scratchend"
+                />
             </div>
         </main>
     )
