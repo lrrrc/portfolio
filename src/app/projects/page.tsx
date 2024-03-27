626ee4cbd714d8caaf5dc9191a75f2abf6c540a6
Leonardo <leonardorecco3@gmail.com>
2024-03-27 09:10:04 -0300

    fix: css bugs

--- src/app/projects/page.tsx
+++ src/app/projects/page.tsx
@@ -6,22 +6,22 @@ export default function Projects() {
     return (
         <main>
             <Header />
-            <div className="grid grid-cols-3 ml-[23rem] mr-[20rem] mt-36 max-2xl:ml-[15%] max-2xl:mr-0 max-2xl:grid-cols-1">
+            <div className="grid gap-4 grid-rows-2 mt-8 justify-center content-center grid-flow-col">
+               <ProjectCard
+                title="My personal AI"
+                content="This AI is designed to provide daily assistance. It's powered by the ChatGPT 4 Turbo API."
+                url="https://ai.leonardocardoso.dev"     
+               />
                 <ProjectCard
-                    title="Shortener URL"
-                    content="This software is a URL shortener that facilitates the LGPD system for some documentation that I need to use on a daily basis. In this project, only JavaScript is used."
-                    url="https://github.com/leozvx/encurtador-tinyurl"
-                />
-                 <ProjectCard
-                    title="Studying Guide"
-                    content="A straightforward guide to enhance your coding skills. This guide covers various programming fundamentals with a particular focus on algorithms and Front-End tips."
-                    url="https://leozvx.github.io/study-pages"
-                />
-                  <ProjectCard
-                    title="scratchend"
-                    content="A new CLI tool to save time in your back-end setup. The function of the CLI is to create all your dependencies and the basic architecture, so you can start your project with all the best practices."
-                    url="https://github.com/scratchend/scratchend"
-                />
+                title="My personal AI"
+                content="This AI is designed to provide daily assistance. It's powered by the ChatGPT 4 Turbo API."
+                url="https://ai.leonardocardoso.dev"     
+               />
+                <ProjectCard
+                title="My personal AI"
+                content="This AI is designed to provide daily assistance. It's powered by the ChatGPT 4 Turbo API."
+                url="https://ai.leonardocardoso.dev"     
+               />
             </div>
         </main>
     )
--- src/components/ui/project-card.tsx
+++ src/components/ui/project-card.tsx
@@ -14,18 +14,18 @@ interface IRequest {
 export default function ProjectCard({ title , content, url } : IRequest) {
 
     return (
-        <Card className="grid grid-rows-[min-content_1fr_min-content] w-[20vw] min-h-[32vh] gap-2 flex-col p-1 mb-5  bg-neutral-700 border-none max-2xl:w-[72vw]">
+        <Card className="grid grid-rows-[min-content_1fr_min-content] w-[20vw] min-h-[32vh] gap-2 flex-col p-1 mb-5  bg-neutral-700 border-none max-2xl:w-[30vw] max-2xl:text-center">
             <CardHeader className="pb-0">
                 <CardTitle className="text-zinc-50">
                     {title}
                 </CardTitle>
                 <Separator />
             </CardHeader>
-            <CardContent className="w-full flex flex-col text-justify text-zinc-50 min-h-[5rem] text-lg leading-snug justify-start items-start relative">
+            <CardContent className="w-full flex flex-col text-justify text-zinc-50 min-h-[5rem] text-lg leading-snug justify-start items-start relative max-2xl:text-center max-2xl:justify-center max-2xl:items-center">
                 {content}
             </CardContent>
             <CardFooter className="flex justify-center">
-                <Link href={url} className="w-[100px] bg-metaColor-100 hover:bg-metaColor-200 text-zinc-50 gap-2 rounded-md flex p-2 justify-center items-center text-sm font-medium">
+                <Link href={url} className="w-[100px] bg-metaColor-100 hover:bg-metaColor-200 text-zinc-50 gap-2 rounded-md flex p-2 justify-center items-center text-sm font-medium max-2xl:gap-0">
                     View <Eye />
                 </Link>
             </CardFooter>
